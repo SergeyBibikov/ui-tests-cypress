8852ca34f3385139f71d7b89fafa764985483e67
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-02-02 12:05:06 +0400

    closing AP banner

--- cypress/pageObjects/homepage.js
+++ cypress/pageObjects/homepage.js
@@ -6,4 +6,8 @@ export class Homepage {
     static changeLanguage(lang) {
         return cy.get(`a[class*="dropdown-item"][data-code="${lang}"]`).click({ force: true })
     }
+
+    static closePhishingBanner() {
+        return cy.get('#Modal1').invoke('remove')
+    }
 }
