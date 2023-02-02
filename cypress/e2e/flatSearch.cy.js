4191ebd3e394e4df174f79720d469f9aabddc5d1
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-02-02 12:11:36 +0400

    Homepage AP banner test

--- cypress/e2e/flatSearch.cy.js
+++ cypress/e2e/flatSearch.cy.js
@@ -1,19 +1,36 @@
+/// <reference types= "cypress" />
+import { Homepage } from "../pageObjects/homepage";
+
+
+
+
 describe('Homepage spec', () => {
-  beforeEach(() => {
-    cy.visit('');
-  })
 
-  it('Anti fishing warning is shown on the homepage', () => {
-    cy.get('img[src*="fishing_popup_ka.png"]').should('be.visible')
-  })
+  context('Anti fishing banner', () => {
 
+    const PHISHING_BANNER_KA = 'img[src*="fishing_popup_ka.png"]'
+    const PHISHING_BANNER_EN = 'img[src*="fishing_popup_en.png"]'
+    const PHISHING_BANNER_RU = 'img[src*="fishing_popup_ru.png"]'
 
-  context('Tests without the antifishing banner', () => {
-    beforeEach(() => {
-      cy.get('#Modal1').invoke('remove')
+    it('Anti phishing banner is shown after the language is changed', () => {
+      cy.visit('');
+      cy.get(PHISHING_BANNER_KA).should('be.visible')
+      Homepage.changeLanguage('en');
+      cy.get(PHISHING_BANNER_EN).should('be.visible')
+      Homepage.changeLanguage('ru');
+      cy.get(PHISHING_BANNER_RU).should('be.visible')
     })
 
-    it('No anti fishing warning is shown with session', function () {
+  })
+
+  context('Propery search', () => {
+    beforeEach(() => {
+      cy.visit('');
+      Homepage.closePhishingBanner();
+    })
+
+    it('Deal type filters should have 5 types', function () {
+
     })
   })
 
