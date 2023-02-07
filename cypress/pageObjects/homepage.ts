ab33a5bd9b033fa99b3f64d0b94a84010d45f1cd
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-02-07 12:10:42 +0400

    PO methods as allure steps

--- cypress/pageObjects/homepage.ts
+++ cypress/pageObjects/homepage.ts
@@ -1,15 +1,29 @@
 type Language = 'en' | 'ru' | 'ka'
 
+function wrapWithAllureStep(stepName: string, commandsToWrap: () => void) {
+    cy.allure().startStep(stepName)
+    commandsToWrap()
+    cy.allure().endStep()
+}
+
 export class Homepage {
 
     static searchBlockHeader = "#search_form .searchblock-text"
 
+    static open() {
+        wrapWithAllureStep("Open homepage", () => cy.visit(''))
+    }
+
     static changeLanguage(newLang: Language) {
-        return cy.get(`a[class*="dropdown-item"][data-code="${newLang}"]`).click({ force: true })
+        wrapWithAllureStep(`Change page language to ${newLang}`, () => {
+            cy
+                .get(`a[class*="dropdown-item"][data-code="${newLang}"]`)
+                .click({ force: true })
+        })
     }
 
     static closePhishingBanner() {
-        return cy.get('#Modal1').invoke('remove')
+        wrapWithAllureStep("Close anti phishing banner", () => cy.get('#Modal1').invoke('remove'))
     }
 
 }
