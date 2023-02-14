4b1ce9e1229b5ab556789e183edb30018f0b60b7
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-02-14 15:58:52 +0400

    debug test

--- cypress/e2e/exercises.cy.ts
+++ cypress/e2e/exercises.cy.ts
@@ -1,7 +1,10 @@
 describe('drag and drop', () => {
-    it('image', { baseUrl: "https://www.w3schools.com/html" }, function () {
+    it('image', { baseUrl: "https://www.w3schools.com/html", pageLoadTimeout: 30000 }, function () {
         cy.visit('/html5_draganddrop.asp')
 
         cy.get("#div1 > img").drag('#div2');
+    })
+    it('Will always fail', () => {
+        cy.wrap(false).should('be.true')
     })
 })
